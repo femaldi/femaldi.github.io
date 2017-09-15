b681c97fb9c50d6f2b937e8f6382a5650f3835a3
femaldi <facundoemaldi@gmail.com>
2017-09-15 12:30:04 -0300

    .

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator){
-	navigator.serviceWorker.register('sw.js');
+	navigator.serviceWorker.register('sw.js').then(reg => reg.update());
 }
 document.querySelector('#boton').addEventListener('click', () => {
 	alert("click");
