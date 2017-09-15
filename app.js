3ccad13346cc0b6faebbf78334da90618833d3aa
femaldi <facundoemaldi@gmail.com>
2017-09-15 14:10:44 -0300

    Pongo app de clima

--- app.js
+++
@@ -1,6 +0,0 @@
-if ('serviceWorker' in navigator){
-	navigator.serviceWorker.register('sw.js').then(reg => reg.update());
-}
-document.querySelector('#boton').addEventListener('click', () => {
-	alert("click");
-});
