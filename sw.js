03b2a868e14e194f09dcdb9c90ed676540d9c1c1
femaldi <facundoemaldi@gmail.com>
2017-09-15 11:27:23 -0300

    .

--- sw.js
+++ sw.js
@@ -26,10 +26,10 @@ self.addEventListener('fetch', event => {
 		event.respondWith(
 			caches.match(event.request).then(cachedResponse => {
 				if (cachedResponse){
-					console.log("Request resuelto desde cache: " + event.request);
+					console.log("Request resuelto desde cache: " + event.request.url);
 					return cachedResponse;
 				}
-				console.log("Request resuelto desde la red: " + event.request);
+				console.log("Request resuelto desde la red: " + event.request.url);
 				return caches.open(RUNTIME).then(cache => {
 					return fetch(event.request).then(response => {
 						return cache.put(event.request, response.clone()).then(() => {
